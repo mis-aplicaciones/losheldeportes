8d737048dc95a723103db54633f5057e5df8bcad
mis-aplicaciones <145235914+mis-aplicaciones@users.noreply.github.com>
2023-09-20 11:51:30 -0500

    Add files via upload

---
+++ video-list.js
@@ -0,0 +1,57 @@
+let allVideos = [
+   {
+      name: "Espn",
+      src: "https://tutlehd.xyz/online.php?a=37981",
+      id: "vid_1"
+   },
+   {
+      name: "Espn 2",
+      src: "https://tutlehd.xyz/online.php?a=37983",
+      id: "vid_2"
+   },
+   {
+      name: "Directv Sports",
+      src: "https://tutlehd.xyz/online.php?a=6",
+      id: "vid_3"
+   },
+   {
+      name: "Directv Sports 2",
+      src: "https://tutlehd.xyz/online.php?a=7",
+      id: "vid_4"
+   },
+   {
+      name: "Espn Premium",
+      src: "https://tutlehd.xyz/online.php?a=5",
+      id: "vid_5"
+   },
+   {
+      name: "Fox Sport",
+      src: "https://tucanaldeportivo.com/foxsportshd.php",
+      id: "vid_6"
+   },
+   {
+      name: "Glassmorphism Calculater UI Design",
+      src: "https://www.youtube.com/embed/zO6zgt-C-vQ",
+      id: "vid_7"
+   },
+   {
+      name: "Creative Our Team Section",
+      src: "https://www.youtube.com/embed/osd8Lqn_oVs",
+      id: "vid_8"
+   },
+   {
+      name: "Filter Text Animation Using Html and CSS",
+      src: "https://www.youtube.com/embed/6SO2deNHoCM",
+      id: "vid_9"
+   },
+   {
+      name: "Vertical Navigation Bar",
+      src: "https://www.youtube.com/embed/hvEa8pnUjCE",
+      id: "vid_10"
+   },
+   {
+      name: "How to make Read More Function",
+      src: "https://www.youtube.com/embed/Bk3Pe5XPgUI",
+      id: "vid_11"
+   }
+]
